dac88d82d07b586963dbf574594e40e76c3e8e13
Administrator <josepcg@rbos.co.uk>
2022-10-26 21:24:31 +0530

    add base module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { HomeComponent } from './home/home.component';
+// import { LoginComponent } from './login/login.component';
+// import { AddUserComponent } from './add-user/add-user.component';
+// import { DisplayUsersComponent } from './display-users/display-users.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // HomeComponent,
+    // LoginComponent,
+    // AddUserComponent,
+    // DisplayUsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
